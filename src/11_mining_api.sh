d7c508185065c93e5ecdec6b15a6e56f9d1bba41
Leonid Beder <leonid.beder@gmail.com>
2017-12-24 19:11:59 +0200

    Added mining API script.

---
+++ src/11_mining_api.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+trap "exit" INT TERM ERR
+trap "kill 0" EXIT
+
+./cleanslate.sh
+
+# Start the nodes.
+NODE_PORT=3000
+NODE_URL="http://localhost:${NODE_PORT}"
+
+node ../dist/11_mining_api.js --port=${NODE_PORT} &
+
+sleep 2
+
+# Submit 2 transactions to the node.
+echo -e && read -n 1 -s -r -p "Submitting transactions. Press any key to continue..." && echo -e
+
+curl -X POST -H "Content-Type: application/json" -d '{
+ "senderAddress": "Alice",
+ "recipientAddress": "Bob",
+ "value": "1000"
+}' "${NODE_URL}/transactions" -w "\n"
+
+curl -X POST -H "Content-Type: application/json" -d '{
+ "senderAddress": "Alice",
+ "recipientAddress": "Eve",
+ "value": "12345"
+}' "${NODE_URL}/transactions" -w "\n"
+
+# Mine one block.
+echo -e && read -n 1 -s -r -p "Mining first block. Press any key to continue..." && echo -e
+
+curl -X POST -H "Content-Type: application/json" "${NODE_URL}/blocks/mine" -w "\n"
+
+# Mine another block.
+echo -e && read -n 1 -s -r -p "Mining second block. Press any key to continue..." && echo -e
+
+curl -X POST -H "Content-Type: application/json" "${NODE_URL}/blocks/mine" -w "\n"
+
+wait
+
