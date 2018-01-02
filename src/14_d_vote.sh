b4c1b3037490ffbd9b28d9c638655b1bb6111b4c
Oren Kedem <orenku@gmail.com>
2018-01-02 23:09:02 +0200

    Change shell script, to run configurable number of nodes, and set voting options

--- src/14_d_vote.sh
+++ src/14_d_vote.sh
@@ -5,31 +5,83 @@ trap "kill 0" EXIT
 
 ./cleanslate.sh
 
-# Start the nodes.
-NODE1="A"
-NODE1_PORT=3000
-NODE1_URL="http://localhost:${NODE1_PORT}"
-NODE2="B"
-NODE2_URL="http://localhost:3001"
-NODE2_PORT=3001
+#get command line options
+# n - Number of voters
+# o- Vote options - a comma seperated string of options
+# h - help message
 
-node ../dist/13_consensus.js --port=${NODE1_PORT} --id=${NODE1} &
-node ../dist/13_consensus.js --port=${NODE2_PORT} --id=${NODE2} &
+#set default options:
+NUMBER_OF_USERS=5;
+VOTE_OPTIONS='yes,no';
+MISSING_ARG='';
+BASE_PORT=3000;
 
-sleep 2
+while getopts :n:o:h option
+do
+ case "${option}"
+ in
+ n) NUMBER_OF_USERS=${OPTARG};;
+ o) VOTE_OPTIONS=${OPTARG};;
+ ?) MISSING_ARG==${OPTARG};;
+ esac
+done
+
+if [ "$MISSING_ARG" != "" ];
+then
+    echo '';
+    echo 'dvote - usage options: ';
+    echo '----------------------';
+    echo '-n : Number of voters (integer)';
+    echo '-o : Voting options - A string of options seperated by comma (default = yes,no)';
+    echo ' ';
+    echo ' example:';
+    echo "dvote -n 15 -o 'yes,no,maybe'"
+    echo ' ';
+    exit;
+fi
+echo 'all is well'
+
+for index in $(eval echo "{0..$((NUMBER_OF_USERS-1))}");
+do
+    echo processing ${index} of ${NUMBER_OF_USERS};
+    echo using port $(($BASE_PORT+$index)) in node "NODE"-${index}
+
+    node ../dist/14_d_vote.js --port=$(($BASE_PORT+$index)) --id="NODE"-${index} &
+    sleep 0.5
+
+done
+
+
+
+
+
 
 # Register the nodes.
 echo -e && read -n 1 -s -r -p "Registering the node. Press any key to continue..." && echo -e
 
-curl -X POST -H "Content-Type: application/json" -d "{
- \"id\": \"${NODE2}\",
- \"url\": \"${NODE2_URL}\"
-}" "${NODE1_URL}/nodes" -w "\n"
+for index in $(eval echo "{0..$((NUMBER_OF_USERS-1))}")
+do
+    NODE_ID="NODE-${index}"
+    NODE_PORT="$(($BASE_PORT+$index))"
+    NODE_URL="http://localhost:${NODE_PORT}"
 
-curl -X POST -H "Content-Type: application/json" -d "{
- \"id\": \"${NODE1}\",
- \"url\": \"${NODE1_URL}\"
-}" "${NODE2_URL}/nodes" -w "\n"
+    for to_index in $(eval echo "{0..$((NUMBER_OF_USERS-1))}")
+    do
+        DESTINATION_URL="http://localhost:$(($BASE_PORT+$to_index))/nodes"
+        if [ $index != $to_index ]
+        then
+            echo registering ${NODE_ID} with port ${NODE_PORT} to node ${NODE_URL};
+
+            curl -v -X POST -H "Content-Type: application/json" -d "{
+                \"id\": \"${NODE_ID}\",
+                \"url\": \"${NODE_URL}\"
+                }" ${DESTINATION_URL} -w "\n"
+        fi
+    done
+done
+
+wait 
+exit
 
 # Submit 2 transactions to the first node.
 echo -e && read -n 1 -s -r -p "Submitting transactions. Press any key to continue..." && echo -e
