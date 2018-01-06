f3ba04f615cd05cb2046e238ec824b05eabec6a0
mrbar42 <mrbar42@gmail.com>
2018-01-06 21:57:52 +0200

    add source maps

--- webpack.config.js
+++ webpack.config.js
@@ -6,69 +6,70 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const extractPlugin = new ExtractTextPlugin({filename: './app.css'});
 
 const config = {
-    context: path.resolve(__dirname, 'src'),
-    entry: {
-        app: ['./app.ts', './app.css']
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'app.js'
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/, loader: 'ts-loader'
-            },
-            {
-                test: /\.css$/,
-                use: extractPlugin.extract({
-                    use: [
-                        'css-loader'
-                    ],
-                    fallback: 'style-loader'
-                })
-            }, {
-                test: /\.(jpg|png|gif|svg)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: './assets/media/'
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: './assets/fonts/'
-                            //publicPath: './assets/fonts/'
-                        }
-                    }
-                ]
+  context: path.resolve(__dirname, 'src'),
+  devtool: 'source-map',
+  entry: {
+    app: ['./app.ts', './app.css']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/, loader: 'ts-loader'
+      },
+      {
+        test: /\.css$/,
+        use: extractPlugin.extract({
+          use: [
+            'css-loader'
+          ],
+          fallback: 'style-loader'
+        })
+      }, {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: './assets/media/'
             }
+          }
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({template: 'app.html'}),
-        extractPlugin,
-        new CleanWebpackPlugin(['dist'])
-    ],
-    devServer: {
-        contentBase: path.resolve(__dirname, './dist/assets/media'),
-        compress: true,
-        port: 12000,
-        stats: 'errors-only',
-        open: false
-    }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: './assets/fonts/'
+              //publicPath: './assets/fonts/'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: 'app.html'}),
+    extractPlugin,
+    new CleanWebpackPlugin(['dist'])
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist/assets/media'),
+    compress: true,
+    port: 12000,
+    stats: 'errors-only',
+    open: false
+  }
 };
 
 module.exports = config;
